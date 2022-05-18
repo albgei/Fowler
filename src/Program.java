40288a30493f1635a4ff751bf5d316dd7cbe7454
albgei <albert@geisbauer.net>
2022-05-18 12:16:45 +0200

    reverted to state of commit 'c0e1c7a'

--- src/Program.java
+++ src/Program.java
@@ -10,9 +10,9 @@ public class Program
     /**
      * This is the main entry point for the application
      */
-    
-    
-    public static void main(String args[]) 
+
+
+    public static void main(String args[])
     {
         String result;
         System.out.println("Welcome to the Movie Store");
@@ -21,11 +21,9 @@ public class Program
         Rental r1 = new Rental(m1, 10);
         Rental r2 = new Rental(m2, 5);
         Customer c1 = new Customer("joe");
-        c1.addRental(r1);   c1.addRental(r2);   
+        c1.addRental(r1);   c1.addRental(r2);
         System.out.println("Let's get the Statement");
         result = c1.statement();
         System.out.println(result);
     }
 }
-
-

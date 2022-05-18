44b8512a0cf7968c9a6b0c46657d4a285397533c
albgei <albert@geisbauer.net>
2022-05-18 12:26:06 +0200

    extracted and moved method getFrequentRenterPoints to Rental final

--- src/Rental.java
+++ src/Rental.java
@@ -12,13 +12,32 @@ class Rental {
         return movie;
     }
 
+
     public double getCharge() {
-        return getMovie().getCharge(getDaysRented());
+        double result = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                result += 2;
+                if (getDaysRented() > 2)
+                    result += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (getDaysRented() > 3)
+                    result += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return result;
     }
 
     public int getFrequentRenterPoints() {
-        return getMovie().getFrequentRenterPoints(getDaysRented());
+        // add frequent renter points
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+        return 1;
     }
-
-
 }
